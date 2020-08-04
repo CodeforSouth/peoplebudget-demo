d7a71a2d3385ddff940b8e6f71bb7b90232bc655
aaaragon <aragonandre333@gmail.com>
2020-08-04 19:54:05 -0400

    popup styling

--- view/src/components/Popup/index.jsx
+++ view/src/components/Popup/index.jsx
@@ -4,28 +4,41 @@ import {
     modal_overlay,
     modal_wrapper,
     modal,
-    modal_header,
-    modal_close_button
+    modal_content,
+    modal_close_button,
+    modal_help,
+    tour_button
 } from './index.module.css';
 
-const Popup = ({ isShowing, hide }) => isShowing ? ReactDOM.createPortal(
-  <React.Fragment>
-    <div className={modal_overlay}/>
-    <div className={modal_wrapper} aria-modal aria-hidden tabIndex={-1} role="dialog">
-      <div className={modal}>
-        <div className={modal_header}>
-          <button type="button" className={modal_close_button} data-dismiss="modal" aria-label="Close" onClick={hide}>
-            <span aria-hidden="true">&times;</span>
-          </button>
-        </div>
-        <p>
-          Need Help?
-          Take the Tour!
-          <button>Video Tour</button>
-        </p>
-      </div>
-    </div>
-  </React.Fragment>, document.body
-) : null;
+const Popup = ({ isShowing, hide }) =>
+    isShowing
+        ? ReactDOM.createPortal(
+              <React.Fragment>
+                  <div className={modal_overlay} />
+                  <div className={modal_wrapper} aria-modal aria-hidden  role="dialog">
+                      <div className={modal}>
+
+                          <div className={modal_content}>
+                              <button
+                                  type="button"
+                                  className={modal_close_button}
+                                  data-dismiss="modal"
+                                  aria-label="Close"
+                                  onClick={hide}
+                              >
+                                  <span aria-hidden="true">&times;</span>
+                              </button>
+                              <div className={modal_help}>
+                                  need help? take the tour
+                              </div>
+                              <button className={tour_button}>Video Tour</button>
+                          </div>
+
+                      </div>
+                  </div>
+              </React.Fragment>,
+              document.body
+          )
+        : null;
 
 export default Popup;
