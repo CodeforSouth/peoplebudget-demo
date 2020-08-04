b20662f85641b8481c12545d348b4623a2f9e554
Earl Cameron <mr.e.cameron@gmail.com>
2020-08-03 22:27:44 -0400

    working  on project list

--- view/src/components/MapView/ProjectCard/index.jsx
+++ view/src/components/MapView/ProjectCard/index.jsx
@@ -1,14 +1,17 @@
 import React, { useState } from 'react';
-import {card} from   './index.module.css'
+import { card } from './index.module.css';
 
 const ProjectCard = (props) => {
-    return <div className={`col-10 mx-auto mb-3 ${card}`}>
-        {props.title}
-        <div></div>
-        <div></div>
-        <div></div>
-        <div></div>
-        </div>;
+    return (
+        <div className={`col-10 mx-auto mb-3 ${card}`}>
+            <div className="row">
+                <div className='col-6'>{props.title}</div>
+                <div className='col-6'>{props.views}</div>
+                <div className='col-6'>{props.filter}</div>
+                <div className='col-6'>{props.upvotes}</div>
+            </div>
+        </div>
+    );
 };
 
 export default ProjectCard;
