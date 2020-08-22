f97e9772e7452cb188f7018a6bbca3b433b49a35
Jeffrey Yuvero <jeffrey.yuvero001@mymdc.net>
2020-08-21 23:34:07 -0400

    Admin Dashboard Logo & Breadcrumb

---
+++ view/src/pages/Dashboard.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import DashboardLogo from '../components/Dashboard Logo';
+
+const Dashboard = () => {
+    return (
+        <div>
+            <DashboardLogo></DashboardLogo>
+        </div>
+    );
+};
+
+export default Dashboard;
