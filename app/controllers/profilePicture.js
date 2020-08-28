24b3fc9bc042e7e39b77185bcf73323c85c848ba
Osmany Pujol <Osmany.Pujolcs@gmail.com>
2020-08-28 16:44:26 -0400

    finished creating the profile picture api

--- app/controllers/profilePicture.js
+++ app/controllers/profilePicture.js
@@ -1,4 +1,4 @@
-const profilePicture = require('../models/profilePicture');
+const profilepicture = require('../models/profilePicture');
 const router = require('express').Router();
 const { customValidator } = require('../helpers/validator');
 const { ClientError } = require('../helpers/error');
@@ -11,10 +11,99 @@ router.post('/', async (req, res, next) => {
         next(validationError);
         return;
     }
-    const {
-        body: { image }
-    } = req;
-    
+    try {
+        const {
+            body: { image }
+        } = req;
+        const newPicture = await profilepicture.create({
+            image: image
+        });
+        res.header('Location', `api/image/v1/?id=${newPicture.id}`);
+        res.statusCode = 201;
+        res.send({ response: 'profile picture created' });
+    } catch (error) {
+        next(new ClientError(400, error.message));
+    }
+});
+router.get('/', async (req, res, next) => {
+    let results = null;
+    try {
+        if (req.query.hasOwnProperty('id')) {
+            results = await profilepicture.findOne({
+                where: {
+                    id: req.query.id
+                },
+                attributes: {
+                    exclude: ['createdAt', 'updatedAt']
+                }
+            });
+        }
+        if (!results) {
+            next(new ClientError(400, `Image id ${req.query.id} doesn't exist`));
+            return;
+        }
+        res.send({ response: results });
+    } catch (error) {
+        next(error);
+    }
+});
+router.put('/', async (req, res, next) => {
+    let result = null;
+    const validationError = customValidator(req.body, {
+        id: null,
+        image: null
+    });
+    if (validationError) {
+        next(validationError);
+        return;
+    }
+    try {
+        const {
+            body: { image }
+        } = req;
+        result = await profilepicture.update(
+            { image },
+            {
+                where: { id: req.query.id }
+            }
+        );
+        if (result.length === 1 && result[0] === 0) {
+            next(new ClientError(400, `id ${req.body.id} doesn't exist`));
+            return;
+        }
+        res.send({ response: `image updated` });
+    } catch (error) {
+        next(error);
+    }
+});
+router.delete('/', async (req, res, next) => {
+    try {
+        let result = null;
+        if (!Array.isArray(req.body.id)) {
+            result = await profilepicture.destroy({
+                where: { id: req.body.id }
+            });
+
+            if (!result) {
+                next(new ClientError(400, `id ${req.body.id} doesn't exist`));
+                return;
+            }
+            res.send({ response: `Id ${req.body.id} was deleted` });
+        } else {
+            result = await profilepicture.destroy({
+                where: { id: req.body.id }
+            });
+            if (!result) {
+                next(new ClientError(400, `ids [${req.body.id.join(', ')}] don't exist`));
+                return;
+            }
+            res.send({
+                response: `images [${req.body.id.join(', ')}] deleted`
+            });
+        }
+    } catch (error) {
+        next(error);
+    }
 });
 
 module.exports = { router, version: 1 };
--- app/models/profilePicture.js
+++ app/models/profilePicture.js
@@ -1,11 +1,11 @@
 const { DB } = require('../database/database');
 const { DataTypes } = require('sequelize');
 
-const profilePicture = DB.define('profilepicture', {
+const profilepicture = DB.define('profilepicture', {
     image: {
         type: DataTypes.STRING,
         allowNull: false
     }
 });
 
-module.exports = profilePicture;
+module.exports = profilepicture;
